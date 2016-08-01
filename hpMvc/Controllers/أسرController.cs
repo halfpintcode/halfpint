3f21b9be24e22cde7989019363fa79468bbede82
jrezuke <j.rezuke@verizon.net>
2016-08-01 09:40:00 -0400

    added spanish, arabic and french family pages

---
+++ hpMvc/Controllers/أسرController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Mvc;
+using hpMvc.DataBase;
+using hpMvc.Models;
+using hpMvc.ViewModels;
+using hpMvc.DataBase;
+
+namespace hpMvc.Controllers
+{
+    public class أسرController : Controller
+    {
+        //
+        // GET: /Families/
+
+        public ActionResult Index()
+        {
+            int count = DbUtils.GetRandomizedSubjectsCount();
+
+            var contact = new FamilyContactsModel();
+
+            var vm = new FamiliesViewModel();
+            vm.RandomizedCount = count;
+            vm.FamilyContact = contact;
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FamiliesViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                FamiliesBusiness.AddFamiliesContact(vm.FamilyContact);
+                var u = new UrlHelper(this.Request.RequestContext);
+                string url = "http://" + Request.Url.Host + u.RouteUrl("Default", new { Controller = "Families", Action = "Index" });
+                
+                FamiliesBusiness.ProcessEmails(vm.FamilyContact,url,Server);
+                
+                return RedirectToAction("ContactConfirmation", "Families", new {fvm = vm});
+            }
+            else
+            {
+                return View(vm);    
+            }
+            
+        }
+
+        public ActionResult ContactConfirmation(FamiliesViewModel fvm)
+        {
+            return View();
+        }
+
+    }
+}
