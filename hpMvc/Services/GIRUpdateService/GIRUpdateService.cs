e6f46bf0545f9bd9c2230029fb3e076b93e3140c
jrezuke <j.rezuke@verizon.net>
2016-08-11 09:58:04 -0400

    fix families - start GIR update service

---
+++ hpMvc/Services/GIRUpdateService/GIRUpdateService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hpMvc.Services.GIRUpdateService
+{
+    public static class GIRUpdateService
+    {
+        public static bool UpdateAll()
+        {
+
+            return true;
+        }
+
+        public static bool UpdateForStudyId(string studyId)
+        {
+            return true;
+        }
+        public static bool UpdateForStudyIdAndDate(string studyID, string calcDate)
+        {
+
+            return true;
+        }
+    }
+}
